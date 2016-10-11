117a99c599f506bab026c26f8d4ab7ef2fee5568
stantoncbradley <stantoncbradley@gmail.com>
2016-10-10 22:11:53 -0700

    add ticker styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,88 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Ticker from './Ticker.js';
+import {
+  PageHeader,
+  Form,
+  FormGroup,
+  FormControl,
+  ControlLabel,
+  Well,
+  Button,
+  Alert,
+  Table
+} from 'react-bootstrap';
+import TickerRow from './TickerRow.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tickers: ['AAPL'],
+      alert: null
+    }
+  }
+
+  _renderAlert() {
+    if (this.state.alert) {
+      return <Alert bsStyle='warning' onDismiss={this._dismissAlert.bind(this)}>{this.state.alert}</Alert>
+    }
+  }
+
+  _dismissAlert() {
+    this.setState({ alert: null})
+  }
+
+  _addTicker(event) {
+    event.preventDefault()
+    const newTicker = event.target.elements[0].value.toUpperCase();
+    if (this.state.tickers.indexOf(newTicker) > -1) {
+      this.setState({
+        alert: `${newTicker} already exists!`
+      })
+      return;
+    }
+    this.setState({
+      alert: null,
+      tickers: this.state.tickers.concat(newTicker)
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        Tickers:
-        <Ticker ticker='AAPL' />
+      <div className="container">
+        <PageHeader style={{textAlign: 'center'}}>Ticker Tracker</ PageHeader>
+        {this._renderAlert()}
+        <Form inline onSubmit={this._addTicker.bind(this)}>
+          <FormGroup>
+            <ControlLabel>Add by ticker:</ControlLabel>
+            {' '}
+            <FormControl
+              type="text"
+              placeholder="ticker"
+            />
+          </FormGroup>
+          {' '}
+          <Button bsStyle="success" type="submit">Add</Button>
+        </Form>
+        <br />
+        <Table striped bordered condensed hover>
+          <thead>
+            <tr>
+              <th>ticker</th>
+              <th>exchange</th>
+              <th>price</th>
+              <th>change</th>
+              <th>change percent</th>
+              <th>last trade time</th>
+              <th>dividend</th>
+              <th>yield</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.state.tickers.map(ticker => (
+              <TickerRow ticker={ticker} key={ticker}/>
+            ))}
+          </tbody>
+        </Table>
       </div>
     );
   }
---
+++ src/TickerRow.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import io from 'socket.io-client';
+
+const domain = 'http://localhost:4000';
+const socket = io(domain);
+
+class TickerRow extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      data: {},
+    }
+  }
+  componentWillMount() {
+    const self = this;
+    socket.emit('ticker', this.props.ticker);
+    socket.on(this.props.ticker, (data) => {
+      const parsedData = JSON.parse(data);
+      console.log(data);
+      self.setState({data: parsedData})
+    });
+  }
+
+  render() {
+    const tickerData = this.state.data;
+    return (
+      <tr>
+        <td>{tickerData.ticker}</td>
+        <td>{tickerData.exchange}</td>
+        <td>{tickerData.price}</td>
+        <td>{tickerData.change}</td>
+        <td>{tickerData.change_percent}</td>
+        <td>{tickerData.last_trade_time}</td>
+        <td>{tickerData.dividend}</td>
+        <td>{tickerData.yield}</td>
+      </tr>
+    );
+  }
+}
+
+export default TickerRow;
